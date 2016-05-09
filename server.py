052573926a800b238c1129bd61085ee45b62658f
Jonas Tingeborn <jonas@tomologic.com>
2016-05-09 13:10:03 +0200

    Add support for chunked transfer
    Flask by itself has issues processing chunked transfers. Gevent however
    can handle it and can work together with flask.
    Therefore the underlying HTTP server is now gevent.

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-import sys
 from flask import Flask, Response
+from gevent.pywsgi import WSGIServer
 import json
 
 app = Flask(__name__)
@@ -9,10 +9,10 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST', 'PUT'])
 def do_post():
     return Response(json.dumps(returndata), mimetype='application/json')
-    return flask.jsonify(**returndata)
 
 if __name__ == "__main__":
     with open('data.json') as f:
         returndata = json.load(f)
     app.debug = True
-    app.run(host='0.0.0.0', port=8080)
+    print("Listening on 8080")
+    server = WSGIServer(('', 8080), app).serve_forever()
